48c92fca773c449da88dad619528364a046e2438
Emlyn <71500044+emlynlj@users.noreply.github.com>
2023-07-22 23:34:20 +0200

    Update placenl-userscript-autoupdater.user.js

--- placenl-userscript-autoupdater.user.js
+++ placenl-userscript-autoupdater.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         PlaceNL Userscript (Autoupdater)
-// @namespace    https://github.com/PlaceNL/Userscript
+// @namespace    emlynlj.co.uk
 // @version      0.0.2
 // @description  The easiest way to run our automated placer and keep it up to date, right from your browser
-// @author       PlaceNL
+// @author       Emlyn, PlaceNL
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @connect      reddit.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/PlaceNL/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
-// @downloadURL  https://github.com/PlaceNL/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
+// @updateURL    https://github.com/SpaceRPlace/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
+// @downloadURL  https://github.com/SpaceRPlace/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
 // @grant        GM.xmlHttpRequest
 // @connect      github.com
 // @connect      objects.githubusercontent.com
